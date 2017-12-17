091a2b2f10f95c27b8a3030918855674986cae19
stamhoffman <stamhoffman@yandex.ru>
2017-12-18 00:43:45 +0300

    Написал функционал для параллельного режима

--- stm_lib/inc/DDS.h
+++ stm_lib/inc/DDS.h
@@ -1,50 +1,32 @@
-#define DDS_PORT   GPIOA
-#define W_CLK      GPIO_Pin_7
-#define FQ_UD      GPIO_Pin_6
-#define DATA_      GPIO_Pin_5
-#define RESET      GPIO_Pin_4
+#define TYPE_CONTROL_PARALLEL // TYPE_CONTROL_SERIAL (default)
 
-#define TYPE_CONTROL_PARALLEL // SERIAL = 1 or PARALLEL = 0(default)
+#define DDS_PORT_CONTROL   GPIOB
+#define W_CLK      GPIO_Pin_0
+#define FQ_UD      GPIO_Pin_1
+#define DATA_      GPIO_Pin_2
+#define RESET      GPIO_Pin_3
 
-void config_port_dds(uint16_t port_control, uint16_t port_data);
-void init_serial_control(void);
-void reset();
-void send_data_serial();
-void send_data_parallel();
-uint32_t calc_registr();
+#ifdef TYPE_CONTROL_PARALLEL
+#define PORT_DATA_DDS GPIOA
+#define BIT_DATA_0 GPIO_Pin_4
+#define BIT_DATA_1 GPIO_Pin_5
+#define BIT_DATA_2 GPIO_Pin_6
+#define BIT_DATA_3 GPIO_Pin_7
+#define BIT_DATA_4 GPIO_Pin_8
+#define BIT_DATA_5 GPIO_Pin_9
+#define BIT_DATA_6 GPIO_Pin_10
+#define BIT_DATA_7 GPIO_Pin_11
+#define BIT_DATA_ALL (BIT_DATA_0 | BIT_DATA_1 | BIT_DATA_2 | BIT_DATA_3 | BIT_DATA_4 | BIT_DATA_5 | BIT_DATA_6 | BIT_DATA_7)
 
+#endif
 
-struct
-{
-	uint32_t delta_phase;
-	uint8_t  phase;
-	uint8_t  sys_clock;
-	uint8_t  fraquent_out;
-} config_out;
+void config_port_dds(void);
+void set_option_dds(uint8_t phase, uint8_t mux6_mode, uint8_t serial_mode,uint8_t power_down);
+void reset(void);
+void send_data_serial(uint32_t control_registr_dds);
+void send_data_parallel(uint32_t control_registr_dds);
+uint32_t calc_registr(double fraquent);
 
-
-typedef struct
-{
-	uint8_t mux6_mode;
-	uint8_t serial_mode;
-	uint8_t power_down;
-	uint8_t ph0;
-	uint8_t ph1;
-	uint8_t ph2;
-	uint8_t ph3;
-	uint8_t ph4;
-} w_control;
-
-typedef struct
-{
-	uint8_t mux6_mode:1;
-	uint8_t serial_mode:1;
-	uint8_t power_down:1;
-	uint8_t ph0:1;
-	uint8_t ph1:1;
-	uint8_t ph2:1;
-	uint8_t ph3:1;
-	uint8_t ph4:1;
-} control;
 
 typedef enum mode_set {Enable = 1,Disable = 0};
+
--- stm_lib/src/DDS.c
+++ stm_lib/src/DDS.c
@@ -1,38 +1,58 @@
 #include "stm32f10x_conf.h"
 
-uint32_t calc_registr(double fraquent)
-{
-uint32_t control_registr_dds = 0;
-control_registr_dds = fraquent * 4294967296.0/180e6;
-return control_registr_dds;
+uint32_t calc_registr(double fraquent) {
+  uint32_t control_registr_dds = 0;
+  control_registr_dds = fraquent * 4294967296.0 / 180e6;
+  return control_registr_dds;
 }
 
-void send_data_parallel(uint32_t control_registr_dds)
-{
-	uint8_t count = 0;
-	for(count = 0; count < 4; control_registr_dds >>=8)
-	{
-
-	}
-
+void send_data_parallel(uint32_t control_registr_dds) {
+  uint8_t count = 0;
+  for (count = 0; count < 4; count++, control_registr_dds >>= 8) {
+    control_registr_dds = control_registr_dds & 0xFF;
+    GPIO_Write(PORT_DATA_DDS, (uint8_t)control_registr_dds);
+    GPIO_SetBits(DDS_PORT_CONTROL, W_CLK);
+    GPIO_ResetBits(DDS_PORT_CONTROL, W_CLK);
+  }
+  GPIO_SetBits(DDS_PORT_CONTROL, FQ_UD);
+  GPIO_ResetBits(DDS_PORT_CONTROL, FQ_UD);
 }
 
-void config_port_dds(uint16_t port_control, uint16_t port_data){
+void config_port_dds() {
 
-#ifdef TYPE_CONTROL_SERIAL
+  if (DDS_PORT_CONTROL == GPIOB) {
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+  }
 
+  GPIO_InitTypeDef GPIO_DDS_CONTROL;
+  GPIO_DDS_CONTROL.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_DDS_CONTROL.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_DDS_CONTROL.GPIO_Pin = W_CLK | FQ_UD | DATA_ | RESET;
+  GPIO_Init(DDS_PORT_CONTROL, &GPIO_DDS_CONTROL);
+
+#ifdef TYPE_CONTROL_PARALLEL
+  if (PORT_DATA_DDS == GPIOA) {
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  }
+
+  GPIO_InitTypeDef GPIO_DDS_DATA;
+  GPIO_DDS_DATA.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_DDS_DATA.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_DDS_DATA.GPIO_Pin = BIT_DATA_ALL;
+  GPIO_Init(PORT_DATA_DDS, &GPIO_DDS_DATA);
 
 #endif
-
-#ifdef 	 TYPE_CONTROL_PARALLEL
-GPIO_TypeDef GPIO_UPR;
-GPIO_UPR.GPIO_Pin = W_CLK;
-
-#endif
-
-
 }
 
-
-
+void set_option_dds(uint8_t phase, uint8_t mux6_mode, uint8_t serial_mode,uint8_t power_down)
+{
+	uint8_t w0 = 0;
+	phase <<= 5;
+	serial_mode <<= 1;
+	power_down <<= 3;
+	w0 = w0 | phase | serial_mode |power_down;
+	GPIO_Write(PORT_DATA_DDS, w0);
+    GPIO_SetBits(DDS_PORT_CONTROL, W_CLK);
+    GPIO_ResetBits(DDS_PORT_CONTROL, W_CLK);
+}
 
